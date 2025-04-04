1b820aae7f9ded0028beb0f99fb1bbd4b4ad5225
Dylan Lozano <dylan-lozano@upc.edu.co>
2025-04-03 23:36:38 -0500

    Vista cuidador

---
+++ app/src/main/java/com/example/loginsignup/actividadesCuidador/BotonesCuidador.java
@@ -0,0 +1,35 @@
+package com.example.loginsignup.actividadesCuidador;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.loginsignup.R;
+import com.example.loginsignup.actividadesVeterinario.AgregarDatosHistoriaMedica;
+
+
+public class BotonesCuidador extends AppCompatActivity{
+    private ImageButton button1;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_botones_cuidador);
+
+        // Inicializa los botones
+        button1 = findViewById(R.id.button1);
+
+
+        // Botón para agregar datos a la historia clínica
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BotonesCuidador.this, AgregarDatosHistoriaMedica.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/loginsignup/actividadesCuidador/MascotasVistaCuidador.java
@@ -0,0 +1,63 @@
+package com.example.loginsignup.actividadesCuidador;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import com.example.loginsignup.R;
+import com.example.loginsignup.actividadesDueño.DueñoSeleccionado;
+import com.example.loginsignup.actividadesDueño.MascotaSeleccionada;
+import com.example.loginsignup.actividadesDueño.MascotasListAdapter;
+import com.example.loginsignup.actividadesVeterinario.BotonesVeterinario;
+import com.example.loginsignup.actividadesVeterinario.Mascotas_vistaVeterinario;
+import com.example.loginsignup.baseDatos.dao.MascotaDao;
+import com.example.loginsignup.baseDatos.entidades.BaseDatos;
+import com.example.loginsignup.baseDatos.entidades.Mascota;
+
+import java.util.List;
+
+public class MascotasVistaCuidador extends AppCompatActivity{
+    private ListView listViewMascotas;
+    private MascotaDao mascotaDao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mascotas_veterinario);
+
+        // Inicialización de la base de datos y el DAO
+        BaseDatos db = Room.databaseBuilder(getApplicationContext(), BaseDatos.class, "aplicacion_db").allowMainThreadQueries().build();
+        mascotaDao = db.mascotaDao();
+
+        int idDueño = DueñoSeleccionado.getInstance().getIdMascota();
+        Log.d("Mascotas_vistaVeterinario", "ID del dueño seleccionado: " + idDueño);
+
+        // Configuración del ListView
+        listViewMascotas = findViewById(R.id.listViewMascotas);
+
+        // Cargar las mascotas desde la base de datos
+        List<Mascota> listaMascotas = mascotaDao.obtenerMascotasDeUsuario(DueñoSeleccionado.getInstance().getIdMascota());
+
+        // Configurar el adaptador para el ListView
+        MascotasListAdapter adapter = new MascotasListAdapter(this, listaMascotas);
+        listViewMascotas.setAdapter(adapter);
+
+        // Configurar el evento de clic en la lista
+        listViewMascotas.setOnItemClickListener((parent, view, position, id) -> {
+            Mascota mascotaSeleccionada = listaMascotas.get(position);
+            MascotaSeleccionada.getInstance().setIdMascota(mascotaSeleccionada.getId_mascota());
+
+            // Acción al hacer clic en una mascota
+            startActivity(new Intent(MascotasVistaCuidador.this, BotonesCuidador.class));
+            Toast.makeText(MascotasVistaCuidador.this, "Seleccionaste: " + MascotaSeleccionada.getInstance().getIdMascota(), Toast.LENGTH_SHORT).show();
+        });
+
+
+    }
+}
